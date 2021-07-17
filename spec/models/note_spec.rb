53b0750fe5833c4757210158ab0bd3df0c088397
ecotiya <ghqyydf829@gmail.com>
2021-07-17 10:38:52 +0900

    3章 モデルスペック実施中

---
+++ spec/models/note_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Note, type: :model do
+  #pending "add some examples to (or delete) #{__FILE__}"
+
+  # 検索文字列に一致するメモを返すこと
+  it "returns notes that match the search term"
+
+end
